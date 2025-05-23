f6dc7e75cea1b5e4c3c14fdd32feef2bf612212b
Abdul Salam <abdulsalamahmed9030@gmail.com>
2025-05-23 19:46:05 +0530

    animated image

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -3,11 +3,18 @@ import Header from '../components/navbar/Header'
 import TaskForceSection from '../components/TaskForceSection'
 import TeamSection from '../components/TeamSection'
 import Footer from '../components/Footer'
+import AboutTwo from '../components/AboutTwo'
+import TaskForceOverview from '../components/TaskForceOverview'
 
 const page = () => {
   return (
     <>
       <Header />
+      <TaskForceOverview
+  subheading="#About Us"
+  heading="Task Force Interiors"
+/>
+      <AboutTwo />
       <TaskForceSection />
       <TeamSection />
       <Footer />
--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -2,11 +2,16 @@ import React from 'react'
 import Header from '../components/navbar/Header'
 import Footer from '../components/Footer'
 import RangeOfServices from '../components/RangeOfServices'
+import TaskForceOverview from '../components/TaskForceOverview'
 
 const page = () => {
   return (
     <>
       <Header />
+      <TaskForceOverview
+  subheading="# Services"
+  heading="Task Force Interiors"
+/>
       <RangeOfServices />
       <Footer />
     </>
